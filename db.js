b8c60807d291746de66eea9b6448640361721e63
pawarfalguni <139803342+pawarfalguni@users.noreply.github.com>
2023-12-11 23:27:45 +0530

    start

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 let mongoose = require("mongoose");
 let url =
-  "mongodb+srv://falgunipawar47:xbBlDjexI2BJHcv6@mern8am.jbvvz5z.mongodb.net/";
+  "mongodb+srv://falgunipawar47:xbBlDjexI2BJHcv6@mern8am.jbvvz5z.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(url);
